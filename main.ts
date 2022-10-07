dcf5eab9e8bde8604bf6134520fe3f4ecc0b1279
genkaikayuna <115199073+genkaikayuna@users.noreply.github.com>
2022-10-07 09:29:59 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.pause(1000)
+    basic.showNumber(1)
+    basic.pause(1000)
+    basic.showNumber(2)
+    basic.pause(1000)
+    basic.showNumber(3)
+    basic.pause(1000)
+    basic.showNumber(4)
+    basic.pause(1000)
+    basic.showNumber(5)
+    basic.pause(1000)
+    basic.showNumber(6)
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+})
+basic.forever(function () {
+	
+})
